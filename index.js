d0d9b6c5ade5b00dcab84c53a51089061efd3d29
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-07-23 11:22:07 +0700

    ...

--- index.js
+++ index.js
@@ -38,3 +38,15 @@ client.on("message_create", async (message) => {
 
 // Start your client
 client.initialize();
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
